5fba5f31b346bb5c6021fcb54c6787aadf047bba
Jovian Dsouza <dsouzajovian123@gmail.com>
2021-01-31 11:46:27 +0530

    ADDED ebot_main package

---
+++ ebot_main/scripts/main_task4.py
@@ -0,0 +1,116 @@
+#! /usr/bin/env python
+
+'''
+This node uses the detection_info topic and performs the actual Ur5 arm manipulation
+'''
+
+import rospy
+import random
+import math
+
+from geometry_msgs.msg import Point, Quaternion, Pose , PointStamped
+from std_msgs.msg import Header
+from object_msgs.msg import ObjectPose
+from std_srvs.srv import Empty
+
+from tf.transformations import quaternion_from_euler
+from tf.transformations import euler_from_quaternion
+
+from ebot_mani.ur5_helper import Ur5Moveit
+
+def roll_angle_to_quaternion(roll_angle):
+    '''Returns a quaternion from roll angle(in deg)'''
+    angle = (math.pi/180) * roll_angle
+    q = quaternion_from_euler(-math.pi+angle, 0 , -math.pi)
+    o = Quaternion()
+    o.x , o.y, o.z, o.w = q[0], q[1], q[2], q[3]
+    return o
+
+class Detect():
+    def __init__(self):
+        self.dict = {}
+        rospy.wait_for_service('/detect')
+        self.detect = rospy.ServiceProxy('detect', Empty)
+        rospy.Subscriber("/detection_info", ObjectPose, self.detect_callback)
+    
+    def detect_callback(self, msg):
+        self.dict[msg.name] = msg.pose.pose.position
+        self.frame_id = msg.pose.header.frame_id
+        rospy.loginfo(">> Detected : " + msg.name)
+
+class Ur5(Ur5Moveit):
+    def __init__(self):
+        Ur5Moveit.__init__(self)
+        self.grasp_quaternion = roll_angle_to_quaternion(32)
+
+    def homePose(self):
+        home_joint_angles = [0.0, -0.5901, 0.0, -1.5708, -0.5235, 1.5708 ]
+        self.set_joint_angles(home_joint_angles)
+    
+    def goToDropBox(self):
+        # dropBoxPose = Pose()
+        # dropBoxPose.position.y = 2.64  - 2.60130259295 - 0.15
+        # dropBoxPose.position.x = 8.2  - 7.67449459357
+        # dropBoxPose.position.z = 1.03
+        # dropBoxPose.orientation = self.arm_group.get_current_pose().pose.orientation
+        # self.go_to_pose(dropBoxPose)
+
+        rand = random.uniform(-0.25, 0.25)
+        angles = [-0.4133827614046668 + rand, 0.13464592230127082, -0.7849043025879245, -0.8762423747543746, -0.6320390724043712, 0.8254714018437523]
+        self.set_joint_angles(angles)
+
+    def graspObject(self, point, width):
+        '''
+        Given the position of object within reach it grasps it.
+        Argument : position (Point msg)
+        '''
+        graspPose = Pose()
+        graspPose.position = point
+        graspPose.position.x -= 0
+        graspPose.position.y -= 0.183 + 0.1
+        graspPose.position.z += 0.12
+        graspPose.orientation = self.grasp_quaternion
+        
+        #Front of object 
+        self.go_to_pose(graspPose)
+
+        #Approach from y axis
+        graspPose.position.y += 0.1
+        self.go_to_pose(graspPose)
+        
+        #Grasp
+        self.closeGripper(width) 
+        rospy.sleep(1)
+
+        #retreat from z axis
+        graspPose.position.z += 0.2
+        self.go_to_pose(graspPose)
+
+def main():
+    pickup_list = ['coke_can', 'battery', 'glue'] #Order in which to pickup items
+    w_dict = {'coke_can': 0.27086, 'battery': 0.26500, 'glue' : 0.31}  # calcalated from x2 - x1 + 0.2
+    transformed_pose_list = []
+    w_list = []
+
+    for model in pickup_list:
+        grasp_point = Point()
+        grasp_point = detect.dict[model]
+        
+        ur5.graspObject(grasp_point, width=w_dict[model])
+        ur5.goToDropBox()
+        ur5.openGripper()
+        ur5.homePose()
+        rospy.sleep(0.1)
+
+        
+
+if __name__ == '__main__':
+    rospy.init_node('grasping_node')
+
+    detect = Detect()
+    detect.detect() #Call the detect service
+
+    ur5 = Ur5()
+    main()
+    del ur5
+
---
+++ ebot_main/scripts/resetModels.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+
+import rospy
+import random
+import math
+from math import pi
+import rospkg
+from tf.transformations import quaternion_from_euler
+
+from gazebo_msgs.srv import GetPhysicsProperties
+from gazebo_msgs.srv import SetPhysicsProperties
+from gazebo_msgs.srv import GetModelState
+from gazebo_msgs.srv import SetModelState
+from gazebo_msgs.srv import SetModelStateRequest
+from gazebo_msgs.srv import SpawnModel, SpawnModelRequest, SpawnModelResponse
+from gazebo_msgs.srv import DeleteModel
+from geometry_msgs.msg import Pose
+from sensor_msgs.msg import PointCloud2
+
+
+def delete_model():
+    delete_model_prox = rospy.ServiceProxy('gazebo/delete_model', DeleteModel)
+    delete_model_prox('training_model')
+
+def create_pose(x, y, z , qx, qy,qz,qw):
+    '''
+    returns a Pose() object from the given x, y, z, qx, qy , qz, qw values
+    '''
+    pose = Pose()
+    pose.position.x = x
+    pose.position.y = y
+    pose.position.z = z
+
+    pose.orientation.x = qx
+    pose.orientation.y = qy
+    pose.orientation.z = qz
+    pose.orientation.w = qw
+    return pose
+
+def create_pose_angle(x, y, z , yaw):
+    '''
+    returns a Pose() object from the given x, y, z, qx, qy , qz, qw values
+    '''
+    pose = Pose()
+    pose.position.x = x
+    pose.position.y = y
+    pose.position.z = z
+
+    q = quaternion_from_euler(0 ,0, yaw)
+    pose.orientation.x = q[0]
+    pose.orientation.y = q[1]
+    pose.orientation.z = q[2]
+    pose.orientation.w = q[3]
+    return pose
+
+def spawn_model(model_name, pose):
+    spawn_srv = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
+    rospy.loginfo("Spawning " + model_name)
+    model_path = rospkg.RosPack().get_path('ebot_gazebo') \
+                    + '/models/' + model_name + '/model.sdf'
+    
+    with open(model_path , 'r') as xml_file:
+        model = xml_file.read()
+    
+    req = SpawnModelRequest()
+    req.model_name = model_name
+    req.model_xml = model
+    req.initial_pose = pose
+
+    spawn_srv.call(req)
+
+def printModelState(model_name):
+    get_model_state_prox = rospy.ServiceProxy('gazebo/get_model_state',GetModelState)
+    model_state = get_model_state_prox(model_name,'world')
+    p = model_state.pose.position
+    q = model_state.pose.orientation
+    print("(%0.8f, %0.8f, %0.8f, %0.8f, %0.8f, %0.8f, %0.8f)" % \
+            (p.x, p.y, p.z, q.x, q.y, q.z, q.w))
+
+def delete_all_model(model_list):
+    delete_model_prox = rospy.ServiceProxy('gazebo/delete_model', DeleteModel)
+    for model in model_list:
+        delete_model_prox(model)
+    delete_model_prox('battery')
+    rospy.sleep(1)
+
+def random_pose(x_min, x_max , y_min, y_max, z=0.9):
+    x = random.uniform(x_min, x_max)
+    y = random.uniform(y_min, y_max)
+    return create_pose_random_orient(x, y, z)
+
+# def spawn_models_random_pose(model_list):
+#     for model in model_list:
+
+
+if __name__ == '__main__':
+    rospy.init_node('capture_node')
+    rospy.wait_for_service('gazebo/get_model_state')
+    rospy.wait_for_service('gazebo/set_model_state')
+    rospy.wait_for_service('gazebo/spawn_sdf_model')
+    rospy.loginfo("Connected to all services!")
+
+    model_list = ['robot_wheels', 'eYIFI', 'soap', 'coke_can', 'water_glass', 'adhesive', 'soap2' , 'glue']
+    realName_dict = {'robot_wheels' : 'robot_wheels' , 
+                     'eYIFI' : 'eYFi_board', 
+                     'soap' : 'FPGA_board' , 
+                     'coke_can' : 'coke_can', 
+                     'water_glass': 'water_glass', 
+                     'adhesive' : 'adhesive', 
+                     'soap2' : 'battery' , 
+                     'glue' : 'glue'}
+
+    delete_all_model(model_list)
+
+    cokePose = create_pose(7.9712791, 3.3939284, 0.8676281, -0.0126091, 0.0003598, 0.0000164, 0.9999204)
+    gluePose = create_pose(7.84000000, 3.23928000, 0.86998147, 0.00000075, -0.00000197, 0.50251043, 0.86457115)
+    batteryPose = create_pose(8.10856002, 3.23999991, 0.87299210, 0.00001689, 0.00000146, 0.00000001, 1.00000000)
+    eYIFIPose = create_pose_angle(7.9712791, 3.2, 1, pi+ pi/4)
+    # spawn_model('coke_can', cokePose)
+    # spawn_model('glue', gluePose)
+    # spawn_model('battery', batteryPose)
+
+    spawn_model('eYIFI', eYIFIPose)
+    # spawn_model('water_glass', gluePose)
+    # spawn_model('adhesive', batteryPose)
+    
+    # printModelState('coke_can')
+    # printModelState('glue')
+    # printModelState('battery')
